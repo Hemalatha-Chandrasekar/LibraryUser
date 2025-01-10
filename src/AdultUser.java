696e141b09d744581083311dac89a706f2641252
Hemalatha-Chandrasekar <hema.alliswell@gmail.com>
2025-01-10 13:11:11 -0600

    Commit

--- src/AdultUser.java
+++ src/AdultUser.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
+public class AdultUser implements LibraryUser {
+    private int age;
+    private String bookType;
 
-public class AdultUser {
+    public AdultUser(int age, String bookType) {
+        this.age = age;
+        this.bookType = bookType;
+    }
+
+    @Override
+    public void registerAccount() {
+        if (age >= 12) {
+            System.out.println("You have successfully registered under an Adult Account.");
+        } else {
+            System.out.println("Sorry, age must be greater than 12 to register as an adult.");
+        }
+    }
+
+    @Override
+    public void requestBook() {
+        if (bookType.equals("Fiction")) {
+            System.out.println("Book issued successfully, please return the book within 7 days.");
+        } else {
+            System.out.println("Oops, you are allowed to take only adult Fiction books.");
+        }
+    }
 }
--- src/LibraryInterfaceDemo.java
+++ src/LibraryInterfaceDemo.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
-
 public class LibraryInterfaceDemo {
+    public static void main(String[] args) {
+        // Test Case #1: KidUser
+        KidUser kid1 = new KidUser(10, "Kids");
+        kid1.registerAccount();
+        kid1.requestBook();
+
+        KidUser kid2 = new KidUser(18, "Fiction");
+        kid2.registerAccount();
+        kid2.requestBook();
+
+        // Test Case #2: AdultUser
+        AdultUser adult1 = new AdultUser(5, "Kids");
+        adult1.registerAccount();
+        adult1.requestBook();
+
+        AdultUser adult2 = new AdultUser(23, "Fiction");
+        adult2.registerAccount();
+        adult2.requestBook();
+    }
 }
